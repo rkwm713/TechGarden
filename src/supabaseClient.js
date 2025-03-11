cbc83be6b3426b0a708fcef86799a4e08aeca9a6
rkwm713 <rkwm713@gmail.com>
2025-03-11 16:13:15 -0500

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://your-project-url.supabase.co';
-const supabaseAnonKey = 'your-anon-key';
+const supabaseUrl = 'https://wqytsssiwhkkyapvvpxw.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndxeXRzc3Npd2hra3lhcHZ2cHh3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE2NDM5MDIsImV4cCI6MjA1NzIxOTkwMn0.08bifJJ2mzgzfsx8n-LdNNbVSwPYqjhiu-esLofFs_Q';
+
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
